6b49c6ac80e2519bcb9eafae10ea0f014717f23a
smit <smitkanani45@gmail.com>
2024-09-26 11:06:52 +0530

    commit1

--- components/main/StarBackground.tsx
+++ components/main/StarBackground.tsx
@@ -2,12 +2,12 @@
 
 import React, { useState, useRef, Suspense } from "react";
 import { Canvas, useFrame } from "@react-three/fiber";
-import { Points, PointMaterial, Preload } from "@react-three/drei";
+import { Points, PointMaterial } from "@react-three/drei";
 // @ts-expect-error: maath has no official types
 import * as random from "maath/random/dist/maath-random.esm";
 
 const StarBackground = (props: JSX.IntrinsicElements["group"]) => {
-  const ref = useRef<any>();
+  const ref = useRef<THREE.Points>(null); // Specify the type as THREE.Points
   const [sphere] = useState(() =>
     random.inSphere(new Float32Array(5000), { radius: 1.2 })
   );
@@ -24,10 +24,10 @@ const StarBackground = (props: JSX.IntrinsicElements["group"]) => {
       <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
         <PointMaterial
           transparent
-          color="#fff" // Corrected color format
+          color="#fff"
           size={0.002}
           sizeAttenuation
-          depthWrite={false} // Corrected spelling
+          depthWrite={false}
         />
       </Points>
     </group>
